ad7d6005ebe9393a93a6dae89699bd3f8d279d0a
Varju Richárd Dániel <varjuricharddaniel@gmail.com>
2024-10-03 13:19:01 +0200

    typescript

---
+++ typescript_gyakorlas/build/alap.ts
@@ -0,0 +1,61 @@
+interface FelhasznaloAlap {
+    nev:string,
+    kor:number,
+    foglalkozas:string
+};
+let felhasznalo:FelhasznaloAlap = {
+    nev:"István",
+    kor: 34,
+    foglalkozas:"tanár"
+};
+let felhasznalo2:FelhasznaloAlap = {
+    nev:"Karcsi",
+    kor: 30,
+    foglalkozas:"tanár"
+};
+console.log(felhasznalo.nev)
+console.log(felhasznalo2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let felhasznalo1:FelhasznaloAlap = {
+    nev:"Karcsi",
+    kor:30,
+    foglalkozas:"tanár"
+}
---
+++ typescript_gyakorlas/build/build/alap.js
@@ -0,0 +1,19 @@
+"use strict";
+;
+let felhasznalo = {
+    nev: "István",
+    kor: 34,
+    foglalkozas: "tanár"
+};
+let felhasznalo2 = {
+    nev: "Karcsi",
+    kor: 30,
+    foglalkozas: "tanár"
+};
+console.log(felhasznalo.nev);
+console.log(felhasznalo2);
+let felhasznalo1 = {
+    nev: "Karcsi",
+    kor: 30,
+    foglalkozas: "tanár"
+};
